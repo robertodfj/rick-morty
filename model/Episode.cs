7846827eb02365a941320a2d303d0a186a94469b
RobertoDFJ <robertodfj93@gmail.com>
2026-02-04 18:37:17 +0100

    Creamos el documento de episode dentro de model

--- model/Episode.cs
+++ model/Episode.cs
@@ -1,4 +1,13 @@
 namespace RickYMorty.model
 {
-    
+    public class Episode
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string AirDate { get; set; }
+        public required string EpisodeCode { get; set; }
+        public required string[] Characters { get; set; }
+        public required string Url { get; set; }
+        public required string Created { get; set; }
+    }
 }
