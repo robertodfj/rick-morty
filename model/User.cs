c2c8c483333716f7e3786212e2fbd5252c506bf8
RobertoDFJ <robertodfj93@gmail.com>
2026-02-04 18:35:32 +0100

    Creamos el documento de user dentro de model

--- model/User.cs
+++ model/User.cs
@@ -1,4 +1,11 @@
 namespace RickYMorty.model
 {
-    
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+        public required string Role { get; set; }
+    }
 }
