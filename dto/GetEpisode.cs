0634c79782cd977fbe15861ab5c8ab38cd8ee173
RobertoDFJ <robertodfj93@gmail.com>
2026-02-04 18:57:58 +0100

    Creamos el fichero GetEpisodeDTO (Lo necesario para cazar ese episodio) en la carpeta dto

--- dto/GetEpisode.cs
+++ dto/GetEpisode.cs
@@ -1,4 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RickYMorty.dto
 {
-    
+    public class GetEpisode
+    {
+        [Required]
+        [Range(1, 51, ErrorMessage = "Id must be between 1 and 51.")] // Maximo de episodios en la API de Rick and Morty
+        public int Id { get; set; }
+        public int OwnerID { get; set; } // Se agregara automaticamente en el backend
+
+        public GetEpisode(int id, int ownerID)
+        {
+            Id = id;
+            OwnerID = ownerID;
+        }
+    }
 }
