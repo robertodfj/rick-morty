5e70b22b92235ba3e9103fd7fd97e3f102318fd6
RobertoDFJ <robertodfj93@gmail.com>
2026-02-05 19:44:21 +0100

    Editamos los ficheros de captura/compra para añadir validaciones

--- dto/GetCharacterDTO.cs
+++ dto/GetCharacterDTO.cs
@@ -5,8 +5,11 @@ namespace RickYMorty.dto
     public class GetCharacterDTO
     {
         [Required]
-        [Range(1, 826, ErrorMessage = "Id must be between 1 and 826.")] // Maximo de personajes en la API de Rick and Morty
+        [Range(1, 826, ErrorMessage = "Id must be between 1 and 826.")] 
         public int Id { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "OwnerID must be a positive integer.")] 
         public int OwnerID { get; set; } // Se agregara automaticamente en el backend
 
         public GetCharacterDTO(int id, int ownerID)
