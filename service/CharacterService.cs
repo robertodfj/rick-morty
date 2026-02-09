93eacb602202c0f5a059a2eda3c84e969bc59f33
RobertoDFJ <robertodfj93@gmail.com>
2026-02-09 09:33:23 +0100

    Añadimos el servicio para obtener personajes con probabilidad por vez trabajada

---
+++ service/CharacterService.cs
@@ -0,0 +1,84 @@
+using RickYMorty.data;
+using RickYMorty.dto;
+
+namespace RickYMorty.service
+{
+    public class CharacterService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AppDBContext _context;
+
+        public CharacterService(HttpClient httpClient, AppDBContext context)
+        {
+            _httpClient = httpClient;
+            _context = context;
+        }
+
+        public async Task<CharacterResponse> CaptureCharacter(GetCharacterDTO getCharacterDTO)
+        {
+            var user = await _context.Users.FindAsync(getCharacterDTO.OwnerID);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            if(!CaptureSuccess(user.TimesWorked ?? 0))
+            {
+                throw new Exception("Capture failed. Keep working to increase your chances!");
+            }
+
+            var response = await _httpClient.GetFromJsonAsync<CharacterResponse>($"https://rickandmortyapi.com/api/character/{getCharacterDTO.Id}");
+                if (response == null)
+                {
+                    throw new Exception("Character not found");
+                }
+
+                var character = new model.Character
+                {
+                    Id = response.Id,
+                    Name = response.Name,
+                    Status = response.Status,
+                    Species = response.Species,
+                    Gender = response.Gender,
+                    ForSale = false,
+                    Price = 0,
+                    OwnedByUserId = getCharacterDTO.OwnerID
+                };
+
+                _context.Characters.Add(character);
+                await _context.SaveChangesAsync();
+
+                var characterResponse = new CharacterResponse
+                {
+                    Id = character.Id,
+                    Name = character.Name,
+                    Status = character.Status,
+                    Species = character.Species,
+                    Gender = character.Gender,
+                    ForSale = character.ForSale,
+                    Price = character.Price
+                };
+
+                return characterResponse;
+
+        }
+
+        public bool CaptureSuccess(int timesWorked)
+        {
+            // Probabilidad base (muy baja)
+            int baseChance = 10;
+
+            // Cada trabajo suma probabilidad
+            int bonusPerWork = 5;
+
+            int chance = baseChance + (timesWorked * bonusPerWork);
+
+            // Capamos para que nunca sea seguro al 100%
+            chance = Math.Min(chance, 90);
+
+            int roll = Random.Shared.Next(1, 101); // 1..100
+
+            return roll <= chance;
+        }
+    }
+}
